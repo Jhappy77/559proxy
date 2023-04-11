23d59f335bf5ce8e46929deb3b5adce583672058
Joel <45266424+Jhappy77@users.noreply.github.com>
2023-04-10 19:30:05 -0600

    improve logging, stop proxy from failing on sync fail

--- src/resyncBadServers.ts
+++ src/resyncBadServers.ts
@@ -7,13 +7,17 @@ export async function resyncBadServers(goodServers: string[], badServers: string
         for (const goodServer of goodServers){
             const reqUrl = `${goodServer}/requestSync`
             console.log(`Attempting reset of ${badServer} via ${goodServer} - reqUrl: ${reqUrl}`)
-            const res = await axios.put(reqUrl, {url: badServer});
-            if(res.status === 200){
-                console.log(`Fixed ${badServer} via ${goodServer}`);
-                fixed = true;
-                break;
-            } else {
-                console.error(`Bad request sync res: ${res.status}`);
+            try {
+                const res = await axios.put(reqUrl, {url: badServer});
+                if(res.status === 200){
+                    console.log(`Fixed ${badServer} via ${goodServer}`);
+                    fixed = true;
+                    break;
+                } else {
+                    console.error(`Bad request sync res: ${res.status}`);
+                }
+            } catch(e) {
+            console.error(`Could not sync ${badServer} via ${goodServer}..`)
             }
         }
         if(!fixed){
