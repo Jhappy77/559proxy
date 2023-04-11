2b32cce47a725233341b8151da3de6d3477d1046
Joel <45266424+Jhappy77@users.noreply.github.com>
2023-04-11 00:07:47 -0600

    Restore initial values

--- src/initialValues.ts
+++ src/initialValues.ts
@@ -1,6 +1,6 @@
-const serverOne = 'http://localhost:8000';
+const serverThree = 'https://appserver559-3.herokuapp.com';
 const serverTwo = `https://appserver559-2.herokuapp.com`;
-const serverThree = `https://appserver559-1.herokuapp.com`
+const serverOne = `https://appserver559-1.herokuapp.com`
 export const initialServerList = [serverOne, serverTwo, serverThree];
 
 const proxyOne = 'https://559proxy.vercel.app';
