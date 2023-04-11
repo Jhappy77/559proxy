0cf2fc6f6676007f490327a73bc5525c1cefb098
Joel <45266424+Jhappy77@users.noreply.github.com>
2023-04-11 05:34:45 -0600

    Use new deployments

--- src/initialValues.ts
+++ src/initialValues.ts
@@ -4,7 +4,7 @@ const serverTwo = `https://appserver559-2.herokuapp.com`;
 const serverOne = `https://appserver559-1.herokuapp.com`
 export const initialServerList = [serverOne, serverTwo, serverThree];
 
-const proxyOne = 'https://559proxy.vercel.app';
+const proxyOne = 'https://proxy1.herokuapp.com';
 // const proxyTwo = 'https://559proxy-2.vercel.app';
 const proxyTwo = 'https://proxy2.herokuapp.com';
 export const initialProxyList = [proxyOne, proxyTwo];
